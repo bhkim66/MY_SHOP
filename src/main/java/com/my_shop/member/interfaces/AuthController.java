e1374fc1c99ecdf6b7a80877bc8a70fee3c22e07
bh <bhkim62@gb-soft.co.kr>
2026-02-07 22:54:38 +0900

    feat: controllers and security config implementation

---
+++ src/main/java/com/my_shop/member/interfaces/AuthController.java
@@ -0,0 +1,29 @@
+package com.my_shop.member.interfaces;
+
+import com.my_shop.common.security.TokenDto;
+import com.my_shop.member.application.MemberService;
+import com.my_shop.member.interfaces.dto.LoginRequest;
+import com.my_shop.member.interfaces.dto.TokenRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final MemberService memberService;
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(memberService.login(request));
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequest request) {
+        return ResponseEntity.ok(memberService.reissue(request));
+    }
+}
